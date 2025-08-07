fc348df285b4b07ae5f32d3695c8d8b9aea87771
Shubham <sak2944@gmail.com>
2025-08-07 22:31:51 +0530

    Day-4

--- Day-4-statelocking/backend.tf
+++ Day-4-statelocking/backend.tf
@@ -3,7 +3,9 @@ terraform {
     bucket = "shubhs-tfstate-bucket"
     key    = "Day-4/terraform.tfstate"              #path of tfstate file
     region = "us-east-1"
-    use_lockfile = true #s3 supports this feature but terraform version should be >1.10
+    #use_lockfile = true #s3 supports this feature but terraform version should be >1.10
+    dynamodb_table = "statelock"
+    encrypt = true
   }
 }
 
