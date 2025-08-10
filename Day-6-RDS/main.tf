57d6f60d24ebfbfe570b4f526012705b598f69e6
Shubham <sak2944@gmail.com>
2025-08-10 19:47:59 +0530

    day-6

--- Day-6-RDS/main.tf
+++ Day-6-RDS/main.tf
@@ -47,11 +47,21 @@ resource "aws_db_instance" "dev_rds" {
   username               = "admin"
   password               = "StrongPassw0rd!"
   skip_final_snapshot    = true
+  publicly_accessible    = true
+  backup_retention_period = 7
+
+  db_subnet_group_name = aws_db_subnet_group.dev_db_subnet_group.name
+  vpc_security_group_ids = [ aws_security_group.dev_rds_sg.id ]
+  depends_on = [ aws_db_subnet_group.dev_db_subnet_group ]
+
+  tags = {
+    Name = "main-rds"
+  }
 }
 
 # Read Replica
 resource "aws_db_instance" "read_replica" {
-  replicate_source_db    = aws_db_instance.dev_rds.identifier  # Use name, not ID
+  replicate_source_db    = aws_db_instance.dev_rds.arn  # Use name, not ID
   instance_class         = "db.t3.micro"
   identifier             = "my-read-replica"
 
