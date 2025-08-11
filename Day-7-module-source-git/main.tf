39c66fb729fc3ef35ab84a9e0df1ca5a30d4d48f
Shubham <sak2944@gmail.com>
2025-08-11 18:19:21 +0530

    Day-7

--- Day-7-module-source-git/main.tf
+++ Day-7-module-source-git/main.tf
@@ -1,4 +1,6 @@
 module "git-source" {
-    source = "value"
+    source = "github.com/sak2944-wqsga/Terraform-git/Day-7-module-source-local"
+    ami-id = "ami-08a6efd148b1f7504"
+    instance-type = "t2.micro"
   
 }
